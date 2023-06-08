6a0bfda4c90ff06b9bf865848781fadc3df3885e
Alondra Sanchez <alomonse1426@gmail.com>
2023-06-08 09:26:52 -0700

    Crud operations with MERN

---
+++ server/model/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: String,
+    status: String
+})
+
+const Users = mongoose.model('users', userSchema);
+
+module.exports = Users;
